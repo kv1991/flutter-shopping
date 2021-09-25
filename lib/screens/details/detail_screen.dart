6bc74efbb8e9d8bf13063de0e887d3b19df8b660
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-09-25 16:20:07 +0800

    feat:  added UI of product detail.

---
+++ lib/screens/details/detail_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// import 'package:Shopping/constants.dart';
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('This is the page of product detail', style: TextStyle(color: Colors.white));
+  }
+}
